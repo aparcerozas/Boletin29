e66ddbbaa6c92d9b15c82c5c0610f08ad770e2b1
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2019-04-10 13:20:00 +0200

    Inicio

---
+++ Boletin29/src/boletin29/Deportiva.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin29;
+
+/**
+ *
+ * @author aparcerozas
+ */
+public class Deportiva extends Barco {
+    private double potencia;
+    private int numCamarotes;
+
+    public Deportiva() {
+    }
+
+    public Deportiva(double potencia, int numCamarotes, String matricula, double eslora) {
+        super(matricula, eslora);
+        this.potencia = potencia;
+        this.numCamarotes = numCamarotes;
+    }
+
+    public double getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+
+    public int getNumCamarotes() {
+        return numCamarotes;
+    }
+
+    public void setNumCamarotes(int numCamarotes) {
+        this.numCamarotes = numCamarotes;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+    @Override
+    public double calcularModulo(){
+        super.setModulo(super.calcularModulo()+(2/potencia + 25/numCamarotes));
+        return super.getModulo();
+    }
+    
+}
